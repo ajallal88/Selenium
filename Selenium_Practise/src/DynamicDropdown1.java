f629acbd53cab700e73ac51baf3563ed227f0f9e
AqibJallal <ajallal88@gmail.com>
2022-09-05 00:39:44 +0530

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I5c06a044284578703604c62d59d4ee725cd6f128

--- Selenium_Practise/src/DynamicDropdown1.java
+++ Selenium_Practise/src/DynamicDropdown1.java
@@ -1,9 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DynamicDropdown1 {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ajallal\\Downloads\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 
 	}
 
